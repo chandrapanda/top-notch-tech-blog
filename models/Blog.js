dc8b010559f258600d1e2a0e8292d2dd5c762efd
Chandra Holt <chandra_holt@hotmail.com>
2022-02-10 16:53:16 -0800

    Added boilerplate code to application.

---
+++ models/Blog.js
@@ -0,0 +1,37 @@
+// TODO: Finish blog entry model
+
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Blog extends Model {}
+
+Blog.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    starting_date: {
+      type: DataTypes.DATE,
+      allowNull: false,
+    },
+    starting_time: {
+      type: DataTypes.TIME,
+      allowNull: false,
+    },
+  },
+  {
+    sequelize,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'gallery',
+  }
+);
+
+module.exports = Blog;
